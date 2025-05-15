fbc79c75f18b3f056352053c5c202cb7d67b2faf
Gabe Lapingcao <mgultra2018@gmail.com>
2025-05-14 17:07:18 -0700

    Initialized SHAPE API via Spring

---
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/BiggerShapeApiApplication.java
@@ -0,0 +1,13 @@
+package bigger.shape.bigger_shape_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiggerShapeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiggerShapeApiApplication.class, args);
+	}
+
+}
---
+++ bigger-shape-api/src/test/java/bigger/shape/bigger_shape_api/BiggerShapeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bigger.shape.bigger_shape_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiggerShapeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
