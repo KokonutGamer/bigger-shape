b992869858a99c96b0de4fa3aa8b0d5095decd97
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-05-15 18:20:27 -0700

    Initial commit - login page

--- bigger-shape-web/src/App.js
+++ bigger-shape-web/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './LoginPage';
 
 function App() {
   return (
---
+++ bigger-shape-web/src/LoginPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const LoginPage = () => {
+    return (
+        <>
+            <h1>This is a test!</h1>
+        </>
+    );
+}
+
+export default LoginPage;
