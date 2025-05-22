28cdc577eb9bcead1b91b2e4887e86f618f60d87
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-05-22 16:47:05 -0700

    create user entity

---
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/entities/User.java
@@ -0,0 +1,49 @@
+package bigger.shape.bigger_shape_api.entities;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.UUID)
+  @Column(name = "id", columnDefinition="uuid")
+  private UUID id;
+
+  @Column(name = "role")
+  private String role;
+
+  @Column(name = "email", unique = true)
+  private String email;
+
+  @Column(name = "phone", unique = true)
+  private String phone;
+
+  @Column(name = "encrypted_password")
+  private String password;
+
+  @Column(name = "created_at")
+  private OffsetDateTime createdAt;
+
+  @Column(name = "is_sso_user")
+  private boolean isSsoUser;
+
+  @Column(name = "is_anonymous")
+  private boolean isAnonymous;
+
+}
