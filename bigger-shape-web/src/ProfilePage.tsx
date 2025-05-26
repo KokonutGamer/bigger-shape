3356ba3d10af3b1d108ead68346bef3d9298e3bb
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-05-26 06:03:49 -0700

    Refactored for readability and added TODOs

--- bigger-shape-web/src/ProfilePage.tsx
+++ bigger-shape-web/src/ProfilePage.tsx
@@ -4,42 +4,18 @@ import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
+  // TODO:
+  // If user is not yet logged in, redirect them to login page
+
   const navigate = useNavigate();
   const [numberOfSubmissions, setNumberOfSubmissions] = useState(3);
-  //   If user is not yet logged in, redirect them to login page
+  // Query the questionnaire submissions related to each specific account
+  // Each submission has their own ID, which can be stored as a URL parameter (11.10)
+  const cards = renderSubmissions(numberOfSubmissions);
+
   useEffect(() => {
     document.title = "Dashboard";
   }, []);
-
-  const cards = [];
-  if (numberOfSubmissions === 0) {
-    cards.push(
-      <CardContainer
-        width={5}
-        height={5}
-        fromColor="gray-100"
-        toColor="gray-100"
-        className="flex flex-row space-x-4"
-      >
-        <p>No submissions yet!</p>
-      </CardContainer>
-    );
-  } else {
-    for (let i = 1; i <= numberOfSubmissions; i++) {
-      cards.push(
-        <CardContainer
-          width={5}
-          height={5}
-          fromColor="gray-100"
-          toColor="gray-100"
-          className="flex flex-row space-x-4
-            hover:shadow-md hover:scale-110 transition-all duration-300"
-        >
-          <p>{`Questionnaire Submission #${i}`}</p>
-        </CardContainer>
-      );
-    }
-  }
 
   const handleTakeQuestionnaire = () => {
     navigate("/survey");
@@ -93,6 +69,42 @@ const ProfilePage = () => {
       </CardContainer>
     </>
   );
+};
+
+// This just accepts an integer parameter for now. Later this should
+// probably be changed to accept an array of Ids which correlate
+// to a unique questionnaire submission
+const renderSubmissions = (numberOfSubmissions: number) => {
+  const cards = [];
+  if (numberOfSubmissions === 0) {
+    cards.push(
+      <CardContainer
+        width={5}
+        height={5}
+        fromColor="gray-100"
+        toColor="gray-100"
+        className="flex flex-row space-x-4"
+      >
+        <p>No submissions yet!</p>
+      </CardContainer>
+    );
+  } else {
+    for (let i = 1; i <= numberOfSubmissions; i++) {
+      cards.push(
+        <CardContainer
+          width={5}
+          height={5}
+          fromColor="gray-100"
+          toColor="gray-100"
+          className="flex flex-row space-x-4
+            hover:shadow-md hover:scale-110 transition-all duration-300"
+        >
+          <p>{`Questionnaire Submission #${i}`}</p>
+        </CardContainer>
+      );
+    }
+  }
+  return cards;
 };
 
 export default ProfilePage;
