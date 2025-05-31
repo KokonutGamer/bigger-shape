dde3b743170d162aac45829119aac44d6c31ce52
Gabe Lapingcao <mgultra2018@gmail.com>
2025-05-30 19:53:50 -0700

    Updated UserController with a test endpoint for JWT verification

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/controllers/UserController.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/controllers/UserController.java
@@ -1,20 +1,33 @@
 package bigger.shape.bigger_shape_api.controllers;
 
-import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import bigger.shape.bigger_shape_api.entities.User;
+import io.jsonwebtoken.Claims;
 
 @RestController
-@RequestMapping("${api.endpoint}")
+@RequestMapping("${api.endpoint}/auth")
 public class UserController {
 
+    private final Logger logger;
+
+    public UserController() {
+        this.logger = LoggerFactory.getLogger(UserController.class);
+    }
+
     @GetMapping("/users")
-    public List<User> me(Authentication authentication) {
-        return null;
+    public Map<String, Object> me(Authentication authentication) {
+        Claims claims = (Claims) authentication.getPrincipal();
+        logger.info("userId: {}", claims.get("sub").toString());
+        return Map.of(
+                "userId", claims.get("sub"),
+                "email", claims.get("email"),
+                "role", claims.get("role"));
     }
 }
