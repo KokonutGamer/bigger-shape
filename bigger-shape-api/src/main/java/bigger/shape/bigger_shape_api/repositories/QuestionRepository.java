b6dc50734326ed86e6a4145a9124f67e8d8ee1e0
Gabe Lapingcao <mgultra2018@gmail.com>
2025-06-03 12:47:21 -0700

    Implemented and updated repositories for answers, users, and questions

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/repositories/QuestionRepository.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/repositories/QuestionRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import bigger.shape.bigger_shape_api.entities.Question;
 
@@ -15,4 +17,14 @@ public interface QuestionRepository extends JpaRepository<Question, UUID> {
    * @return the order of the questions, their type, and content.
    */
   public List<Question> findAllByOrderByOrderAsc();
+
+  /**
+   * Find the UUID of a Question based on the order it appears in. The order a
+   * Question appears in is guaranteed to be unique.
+   * 
+   * @param order The order a Question appears in the Questionnaire.
+   * @return The UUID associated with this Question.
+   */
+  @Query("SELECT q.id FROM Question q WHERE q.order = :order")
+  public UUID findIdByOrder(@Param("order") Long order);
 }
