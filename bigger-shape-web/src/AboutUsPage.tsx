7dd3b9bdfdd2d1b9018e32ad5df3ad35b548aa24
David P <davidhunterphillips03@gmail.com>
2025-06-03 12:52:50 -0700

    small change

--- bigger-shape-web/src/AboutUsPage.tsx
+++ bigger-shape-web/src/AboutUsPage.tsx
@@ -28,24 +28,23 @@ function AboutUsPage() {
                 <div className="flex flex items-center justify-around h-[90vh] w-[100vw]">
 
 
-                    <div className="border-4 border-blue-500 w-[40vw] h-[60vh] rounded-lg p-[2vh] flex justify-center items-center flex-col">
-                        <p className="text-2xl font-bold text-black">
-                            The purpose of this website is to use the skills we have developed in our <a href="https://www.uwb.edu/stem/undergraduate/majors/bscsse">CSSE</a> program
+                    <div className="bg-gradient-to-br from-blue-500 to-blue-200 w-[40vw] h-[60vh] rounded-lg p-[2vh] flex justify-center items-center flex-col">
+                        <p className="text-2xl font-bold text-white">
+                            The purpose of this website is to use the skills we have developed in our <a href="https://www.uwb.edu/stem/undergraduate/majors/bscsse" className="text-blue-500 hover:underline hover:text-blue-700">CSSE</a> program
                             to try and provide resources for homeless people in the Seattle area.
-
                         </p>
 
-                        <p className="text-2xl font-bold text-black mt-[2vh]">
+                        <p className="text-2xl font-bold text-white mt-[2vh]">
                             The goal is to create a tool that is easy for people with varying technical
                             skills to use to find resources for their needs.
                         </p>
                     </div>
-                    <div className="border-4 border-blue-500 w-[40vw] h-[60vh] rounded-lg p-[2vh] flex justify-center items-center flex-col">
-                        <p className="text-xl font-bold text-black italic">The Team</p>
+                    <div className="bg-gradient-to-br from-blue-500 to-blue-200 text-white w-[40vw] h-[60vh] rounded-lg p-[2vh] flex justify-center items-center flex-col">
+                        <p className="text-2xl font-bold text-white italic">The Team</p>
+                        <TeamMember name="Alan Cordova" github="https://github.com/YoloMcFroyo" linkedin="https://www.linkedin.com/in/davidphillips7/" />
                         <TeamMember name="David Phillips" github="https://github.com/orionsView" linkedin="https://www.linkedin.com/in/davidphillips7/" />
                         <TeamMember name="Gabe Lapingcao" github="https://github.com/KokonutGamer" linkedin="https://www.linkedin.com/in/davidphillips7/" />
                         <TeamMember name="Kent Mayoya" github="https://github.com/KentMayoya" linkedin="https://www.linkedin.com/in/davidphillips7/" />
-                        <TeamMember name="Alan Cordova" github="https://github.com/YoloMcFroyo" linkedin="https://www.linkedin.com/in/davidphillips7/" />
 
                     </div>
                 </div>
--- bigger-shape-web/src/components/TeamMember.tsx
+++ bigger-shape-web/src/components/TeamMember.tsx
@@ -3,7 +3,7 @@ function TeamMember(props) {
         <>
             <div className="flex justify-center items-center w-[100%]">
                 <div className="flex justify-start items-center pt-[2vh] w-[30%]">
-                    <p className="text-md text-black mr-[2vh]">{props.name}</p>
+                    <p className="text-md text-white mr-[2vh]">{props.name}</p>
                 </div>
                 <div className="flex justify-end items-center pt-[2vh] w-[30%]">
                     <a href={props.github} className="w-[3vw]">
