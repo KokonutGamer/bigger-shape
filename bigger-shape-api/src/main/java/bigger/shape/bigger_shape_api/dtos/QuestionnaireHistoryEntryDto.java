06ac8366dc6d6d07d11ad3729ee9f8c3d3724eab
Gabe Lapingcao <mgultra2018@gmail.com>
2025-06-03 12:46:43 -0700

    Updated DTOs with new and non-null fields

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/QuestionnaireHistoryEntryDto.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/QuestionnaireHistoryEntryDto.java
@@ -1,11 +1,22 @@
 package bigger.shape.bigger_shape_api.dtos;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import bigger.shape.bigger_shape_api.entities.QuestionnaireResult;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class QuestionnaireHistoryEntryDto {
+    @NonNull
     private QuestionnaireResultDto questionnaire;
     private List<AnswerRecommendationDto> answers;
+
+    public static QuestionnaireHistoryEntryDto fromQustionnaireResultEntity(QuestionnaireResult result) {
+        QuestionnaireHistoryEntryDto dto = new QuestionnaireHistoryEntryDto(
+                new QuestionnaireResultDto(result.getDateTaken(), result.getRiskScore()));
+        dto.setAnswers(new ArrayList<>());
+        return dto;
+    }
 }
--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/QuestionnaireResultDto.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/QuestionnaireResultDto.java
@@ -3,9 +3,14 @@ package bigger.shape.bigger_shape_api.dtos;
 import java.time.OffsetDateTime;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class QuestionnaireResultDto {
+
+    @NonNull
     private OffsetDateTime dateTaken;
+
+    @NonNull
     private String riskScore;
 }
