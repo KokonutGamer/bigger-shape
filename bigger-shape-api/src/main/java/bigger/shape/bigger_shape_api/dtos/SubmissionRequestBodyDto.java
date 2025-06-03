d35e072461c59ca0f47e391a95924225e101786a
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-06-03 15:12:18 -0700

    Add SubmissionRequestBodyDto class for handling submission requests

---
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/SubmissionRequestBodyDto.java
@@ -0,0 +1,14 @@
+package bigger.shape.bigger_shape_api.dtos;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class SubmissionRequestBodyDto {
+  private List<SubmittedAnswerDto> answers;
+
+  public SubmissionRequestBodyDto(List<SubmittedAnswerDto> answers) {
+    this.answers = answers;
+  }
+}
