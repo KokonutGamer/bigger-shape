bd4d485516aa7640eb5593307719c78aa5cdac01
David P <davidhunterphillips03@gmail.com>
2025-06-02 19:50:44 -0700

    added contact info to about page

--- bigger-shape-web/src/AboutUsPage.tsx
+++ bigger-shape-web/src/AboutUsPage.tsx
@@ -36,20 +36,16 @@ function AboutUsPage() {
                         </p>
 
                         <p className="text-2xl font-bold text-black mt-[2vh]">
-                            The goal is to create a tool that easy to use for people to find resources for their
-                            needs. with varying technical skills.
+                            The goal is to create a tool that is easy for people with varying technical
+                            skills to use to find resources for their needs.
                         </p>
                     </div>
                     <div className="border-4 border-blue-500 w-[40vw] h-[60vh] rounded-lg p-[2vh] flex justify-center items-center flex-col">
                         <p className="text-xl font-bold text-black italic">The Team:</p>
-                        <TeamMember name="David Phillips" github="https://github.com/davidphillips7" linkedin="https://www.linkedin.com/in/davidphillips7/" />
-                        <TeamMember name="Gabe Lapingcao" github="https://github.com/davidphillips7" linkedin="https://www.linkedin.com/in/davidphillips7/" />
-                        <TeamMember name="Kent Mayoya" github="https://github.com/davidphillips7" linkedin="https://www.linkedin.com/in/davidphillips7/" />
-                        <TeamMember name="Alan Cordova" github="https://github.com/davidphillips7" linkedin="https://www.linkedin.com/in/davidphillips7/" />
-
-
-
-
+                        <TeamMember name="David Phillips" github="https://github.com/orionsView" linkedin="https://www.linkedin.com/in/davidphillips7/" />
+                        <TeamMember name="Gabe Lapingcao" github="https://github.com/KokonutGamer" linkedin="https://www.linkedin.com/in/davidphillips7/" />
+                        <TeamMember name="Kent Mayoya" github="https://github.com/KentMayoya" linkedin="https://www.linkedin.com/in/davidphillips7/" />
+                        <TeamMember name="Alan Cordova" github="https://github.com/YoloMcFroyo" linkedin="https://www.linkedin.com/in/davidphillips7/" />
 
                     </div>
                 </div>
