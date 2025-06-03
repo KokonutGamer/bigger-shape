d5344fa2b56ad12e4e07479dfdad18a53447a314
David P <davidhunterphillips03@gmail.com>
2025-06-03 16:09:37 -0700

    changed survey to use end point, fixed cors issue

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/controllers/QuestionnaireController.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/controllers/QuestionnaireController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import bigger.shape.bigger_shape_api.responses.GetQuestionsResponse;
 import bigger.shape.bigger_shape_api.services.QuestionnaireService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 /**
  * MIGHT HAVE TO PUT CORS HERE - not sure about it
  * 
  * @CrossOrigin(origins = "http://localhost:_____")
  */
+
 @RequestMapping("/api/v1")
 public class QuestionnaireController {
   private final QuestionnaireService questionnaireService;
@@ -26,6 +29,7 @@ public class QuestionnaireController {
    * Endpoint to get all questions from the questionnaire.
    * 
    * Test on: "http://localhost:8080/api/v1/public/questions"
+   * 
    * @return ResponseEntity containing GetQuestionsResponse with all questions.
    */
   @GetMapping("/public/questions")
