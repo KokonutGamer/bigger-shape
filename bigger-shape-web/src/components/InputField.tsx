f8485ac555d056419680f1c8551bcdadf83be8a6
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-06-04 09:12:38 -0700

    Implemented basic login using email and password

--- bigger-shape-web/src/components/InputField.tsx
+++ bigger-shape-web/src/components/InputField.tsx
@@ -1,29 +1,44 @@
-function InputField({ type, id, label, options, requiredField, regex, setIsValidInput, sizeRange, setIsValidLength, setCurrentValue, currentValue }) {
+function InputField({
+  type,
+  id,
+  label,
+  options,
+  requiredField,
+  regex,
+  setIsValidInput,
+  sizeRange,
+  setIsValidLength,
+  setCurrentValue,
+  currentValue,
+}) {
   function checkInput(event) {
     if (sizeRange) {
-      if (event.target.value.length < sizeRange.min || event.target.value.length > sizeRange.max) {
-        setIsValidLength(prevState => ({
+      if (
+        event.target.value.length < sizeRange.min ||
+        event.target.value.length > sizeRange.max
+      ) {
+        setIsValidLength((prevState) => ({
           ...prevState,
-          [id]: false
+          [id]: false,
         }));
       } else {
-        setIsValidLength(prevState => ({
+        setIsValidLength((prevState) => ({
           ...prevState,
-          [id]: true
+          [id]: true,
         }));
       }
     }
     // console.log("keyup");
     const value = event.target.value;
     if (!requiredField && value === "") {
-      setIsValidInput(prevState => ({
+      setIsValidInput((prevState) => ({
         ...prevState,
-        [id]: true
+        [id]: true,
       }));
     } else if (regex) {
-      setIsValidInput(prevState => ({
+      setIsValidInput((prevState) => ({
         ...prevState,
-        [id]: regex.test(value)
+        [id]: regex.test(value),
       }));
     }
   }
@@ -32,29 +47,48 @@ function InputField({ type, id, label, options, requiredField, regex, setIsValid
     setCurrentValue(event.target.value);
   }
 
-  const labelClass = 'text-left pb-[2vh]';
+  const labelClass = "text-left pb-[2vh]";
   // console.log(type, id, label, options);
   switch (type.toLowerCase()) {
-    case 'text':
-    case 'password':
+    case "text":
+    case "password":
       return (
         <div className="flex mt-[2vh]">
-          <label htmlFor={id} className="pr-[1vh] text-white min-w-[10vw]">{label}</label>
-          <input type={type} id={id} name={id} className="bg-white rounded text-center" required={requiredField} onChange={(e) => {
-            checkInput(e);
-            updateCurrentValue(e);
-          }}></input><p><span className="ml-2 text-red-600">{requiredField ? "*" : " "}</span></p>
+          <label htmlFor={id} className="pr-[1vh] text-white min-w-[10vw]">
+            {label}
+          </label>
+          <input
+            type={type}
+            id={id}
+            name={id}
+            className="bg-white rounded text-center"
+            required={requiredField}
+            onChange={(e) => {
+              checkInput(e);
+              updateCurrentValue(e);
+            }}
+          ></input>
+          <p>
+            <span className="ml-2 text-red-600">
+              {requiredField ? "*" : " "}
+            </span>
+          </p>
           <br></br>
-        </div >
+        </div>
       );
-    case 'select':
+    case "select":
       if (options === null || label === null) {
         return null;
       }
       return (
         <>
           <label className={labelClass}>{label}</label>
-          <select id={id} className="bg-white" onChange={updateCurrentValue} value={currentValue || "default"}>
+          <select
+            id={id}
+            className="bg-white"
+            onChange={updateCurrentValue}
+            value={currentValue || "default"}
+          >
             <option value="default" disabled>
               Select Option
             </option>
