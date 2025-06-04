43c24298b54cab3578806e0ba4c0fc8dba187b0d
Gabe Lapingcao <mgultra2018@gmail.com>
2025-06-03 17:22:19 -0700

    Implemented findAllQuestionnaireHistoryEntries from the id of a User

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/services/UserService.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/services/UserService.java
@@ -2,11 +2,13 @@ package bigger.shape.bigger_shape_api.services;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import bigger.shape.bigger_shape_api.dtos.AnswerDto;
 import bigger.shape.bigger_shape_api.dtos.QuestionnaireHistoryEntryDto;
+import bigger.shape.bigger_shape_api.dtos.QuestionnaireResultDto;
 import bigger.shape.bigger_shape_api.entities.Answer;
 import bigger.shape.bigger_shape_api.entities.AnswerId;
 import bigger.shape.bigger_shape_api.entities.QuestionnaireResult;
@@ -33,11 +35,30 @@ public class UserService {
         this.questionRepository = questionRepository;
     }
 
-    public List<QuestionnaireHistoryEntryDto> getQuestionnaireHistory(UUID id) {
+    public List<QuestionnaireHistoryEntryDto> findAllQuestionnaireHistoryEntries(UUID id) {
+        // Check if the user exists
+        userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        // Get all of the quesionnaire results
         List<QuestionnaireResult> history = questionnaireRepository.findAllQuestionnaireResultsById(id);
+
+        // Process each result: map into a DTO, add all Answers, and collect
         return history.stream()
                 .map(QuestionnaireHistoryEntryDto::fromQuestionnaireResultEntity)
-                .toList();
+                .peek(entry -> {
+                    // Get the quesionnaire DTO
+                    QuestionnaireResultDto dto = entry.getQuestionnaire();
+
+                    // Retrieve answers as a DTO
+                    List<AnswerDto> answers = answerRepository
+                            .findAllAnswersByIdDateScore(id, dto.getDateTaken(), dto.getRiskScore()).stream()
+                            .map(AnswerDto::fromEntity).collect(Collectors.toList());
+
+                    // Add all answers for this entry
+                    entry.getAnswers().addAll(answers);
+                })
+                .collect(Collectors.toList());
+
     }
 
     @Transactional
