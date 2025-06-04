f37dba953c39523d6c31f1d1cae0b874e68ddf25
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-06-03 19:37:59 -0700

    Add questionId field to RecommendationDto and update constructor

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/RecommendationDto.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/dtos/RecommendationDto.java
@@ -4,12 +4,14 @@ import lombok.Data;
 
 @Data
 public class RecommendationDto {
+  private String questionId;
   private String name;
   private String description;
   private String websiteUrl;
   private String contactUrl;
-  
-  public RecommendationDto(String name, String description, String websiteUrl, String contactUrl) {
+
+  public RecommendationDto(String questionId, String name, String description, String websiteUrl, String contactUrl) {
+    this.questionId = questionId;
     this.name = name;
     this.description = description;
     this.websiteUrl = websiteUrl;
