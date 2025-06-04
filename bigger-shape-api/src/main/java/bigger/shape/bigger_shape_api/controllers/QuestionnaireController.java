83785784ec35faddc30305dc6c739bcb639f8217
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-06-03 19:37:48 -0700

    Refactor QuestionnaireController to use @Autowired for QuestionnaireService and clean up CORS comments

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/controllers/QuestionnaireController.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/controllers/QuestionnaireController.java
@@ -1,31 +1,31 @@
 package bigger.shape.bigger_shape_api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bigger.shape.bigger_shape_api.dtos.SubmissionRequestBodyDto;
 import bigger.shape.bigger_shape_api.responses.GetQuestionsResponse;
+import bigger.shape.bigger_shape_api.responses.RecommendationsResponse;
 import bigger.shape.bigger_shape_api.services.QuestionnaireService;
 
 @RestController
-/**
- * MIGHT HAVE TO PUT CORS HERE - not sure about it
- * 
- * @CrossOrigin(origins = "http://localhost:_____")
- */
+//@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1")
 public class QuestionnaireController {
-  private final QuestionnaireService questionnaireService;
-
-  public QuestionnaireController(QuestionnaireService questionnaireService) {
-    this.questionnaireService = questionnaireService;
-  }
+  @Autowired
+  QuestionnaireService questionnaireService;
 
   /**
    * Endpoint to get all questions from the questionnaire.
    * 
    * Test on: "http://localhost:8080/api/v1/public/questions"
+   * 
    * @return ResponseEntity containing GetQuestionsResponse with all questions.
    */
   @GetMapping("/public/questions")
@@ -35,4 +35,20 @@ public class QuestionnaireController {
     return ResponseEntity.ok(result);
   }
 
+  /**
+   * Endpoint recieves questions and answers to the questionnaire
+   * and sends back reponse with appropriate list of recommendations
+   * 
+   * 
+   * Test on: "http://localhost:8080/api/v1/public/submit-answers"
+   * 
+   * @param request
+   * @return
+   */
+  @PostMapping("/public/submit-answers")
+  public ResponseEntity<RecommendationsResponse> submitAnswersAndGetRecommendations(
+      @RequestBody SubmissionRequestBodyDto request) {
+        RecommendationsResponse response = questionnaireService.processAnswersAndGetRecommendations(request);
+    return ResponseEntity.ok(response);
+  }
 }
