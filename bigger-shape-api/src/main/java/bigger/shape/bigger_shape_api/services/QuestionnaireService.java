60e4477824e949cb546dd18d825a1abf05717372
Gabe Lapingcao <mgultra2018@gmail.com>
2025-06-09 21:55:59 -0700

    Refactored questionnaire related objects

--- bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/services/QuestionnaireService.java
+++ bigger-shape-api/src/main/java/bigger/shape/bigger_shape_api/services/QuestionnaireService.java
@@ -10,7 +10,6 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import bigger.shape.bigger_shape_api.dtos.QuestionsDto;
@@ -27,127 +26,136 @@ import bigger.shape.bigger_shape_api.responses.RecommendationsResponse;
 
 @Service
 public class QuestionnaireService {
-  @Autowired
-  private QuestionRepository questionRepository;
 
-  @Autowired
-  private QuestionOptionRepository questionOptionRepository;
+	private final QuestionRepository questionRepository;
+	private final QuestionOptionRepository questionOptionRepository;
+	private final RecommendationRepository recommendationRepository;
+	private final Map<String, Set<Integer>> acceptableAnswers;
 
-  @Autowired
-  private RecommendationRepository recommendationRepository;
+	public QuestionnaireService(QuestionRepository questionRepository,
+			QuestionOptionRepository questionOptionRepository,
+			RecommendationRepository recommendationRepository) {
+		this.questionRepository = questionRepository;
+		this.questionOptionRepository = questionOptionRepository;
+		this.recommendationRepository = recommendationRepository;
 
-  public List<QuestionsDto> getAllQuestionsAsDtos() {
-    List<Question> questions = questionRepository.findAllByOrderByOrderAsc();
+		// Acceptable answers
+		this.acceptableAnswers = new HashMap<>();
 
-    if (questions.isEmpty()) {
-      return Collections.emptyList();
-    }
+		// Monthly rent
+		acceptableAnswers.put("068967bf-3f28-4306-ac07-688c29efc3f8", Set.of(5, 6, 7));
 
-    List<UUID> questionIds = questions.stream()
-        .map(Question::getId)
-        .collect(Collectors.toList());
+		// Disability status
+		acceptableAnswers.put("2109f4b9-50f9-46f3-bebd-deb6351adb64", Set.of(3));
 
-    List<QuestionOption> questionOptions = questionOptionRepository.findByQuestion_IdInOrderById_OrderAsc(questionIds);
+		// Age Range
+		acceptableAnswers.put("32cd1863-adc4-4a08-81f3-cc53ef630607", Set.of(1, 2, 3, 4));
 
-    Map<UUID, List<QuestionOption>> optionsByQuestionId = questionOptions.stream()
-        .collect(Collectors.groupingBy(option -> option.getQuestion().getId()));
+		// Income Range
+		acceptableAnswers.put("494be9d4-2a36-4868-b120-b1c7e6542cb8", Set.of(5, 6, 7, 8));
 
-    return questions.stream()
-        .map(question -> {
-          List<QuestionOption> options = optionsByQuestionId.getOrDefault(question.getId(), Collections.emptyList());
-          List<String> optionContents = options.stream()
-              .map(QuestionOption::getContent)
-              .collect(Collectors.toList());
+		// Number of dependents
+		acceptableAnswers.put("5b36db85-7954-4c3c-bedc-2d24217d04d0", Set.of(3, 4, 5));
 
-          return new QuestionsDto(
-              question.getQuestionType().getName(),
-              question.getId().toString(),
-              question.getContent(),
-              optionContents);
-        })
-        .collect(Collectors.toList());
-  }
+		// Monthly additional spending
+		acceptableAnswers.put("8d651497-48de-450d-93ec-64506b9e9563", Set.of(1, 2, 3, 4));
 
+		// Zip code
+		acceptableAnswers.put("9e3efa78-7bf8-4b88-b93e-715454583779", Set.of(1, 2, 3, 4, 5, 6, 7));
 
-  /**
-   * Processes the answers submitted by the user and returns a list of recommendations.
-   * If the answers are not in the pool of answers, then they indicate that the user is
-   * struggling and needs to resources linked to the question.
-   * 
-   * @param SubmissionRequestBodyDto The request containing the user's answers.
-   * @return RecommendationsResponse containing a list of recommendations based on the user's answers.
-   */
-  public RecommendationsResponse processAnswersAndGetRecommendations(SubmissionRequestBodyDto answerSubmissionRequest) {
-    
-    //Answers that indicate little-no struggle
-    Map<String, Set<Integer>> acceptableAnswers = new HashMap<>();
+		// Average drinks per week
+		acceptableAnswers.put("b8eaec80-6f95-4246-b775-5e684a236f04", Set.of(1, 2));
 
-    //Monthly rent
-    acceptableAnswers.put("068967bf-3f28-4306-ac07-688c29efc3f8", Set.of(5, 6, 7));
+		// Family availability
+		acceptableAnswers.put("ca7e7d4d-3406-4684-b049-2adb96678815", Set.of(1));
 
-    //Disability status
-    acceptableAnswers.put("2109f4b9-50f9-46f3-bebd-deb6351adb64", Set.of(3));
+		// Monthly food cost
+		acceptableAnswers.put("fa35c3c1-26ae-430d-aacd-128da6388060", Set.of(1, 2, 3, 4));
+	}
 
-    // Age Range
-    acceptableAnswers.put("32cd1863-adc4-4a08-81f3-cc53ef630607", Set.of(1, 2, 3, 4));
+	public List<QuestionsDto> getAllQuestionsAsDtos() {
+		List<Question> questions = questionRepository.findAllByOrderByOrderAsc();
 
-    // Income Range
-    acceptableAnswers.put("494be9d4-2a36-4868-b120-b1c7e6542cb8", Set.of(5, 6, 7, 8));
-    
-    //Number of dependents
-    acceptableAnswers.put("5b36db85-7954-4c3c-bedc-2d24217d04d0", Set.of(3, 4, 5));
+		if (questions.isEmpty()) {
+			return Collections.emptyList();
+		}
 
-    //Monthly additional spending
-    acceptableAnswers.put("8d651497-48de-450d-93ec-64506b9e9563", Set.of(1, 2, 3, 4));
+		List<UUID> questionIds = questions.stream()
+				.map(Question::getId)
+				.collect(Collectors.toList());
 
-    //Zip code
-    acceptableAnswers.put("9e3efa78-7bf8-4b88-b93e-715454583779", Set.of(1, 2, 3, 4, 5, 6, 7));
+		List<QuestionOption> questionOptions = questionOptionRepository
+				.findByQuestion_IdInOrderById_OrderAsc(questionIds);
 
-    //Average drinks per week
-    acceptableAnswers.put("b8eaec80-6f95-4246-b775-5e684a236f04", Set.of(1, 2));
+		Map<UUID, List<QuestionOption>> optionsByQuestionId = questionOptions.stream()
+				.collect(Collectors.groupingBy(option -> option.getQuestion().getId()));
 
-    //Family availability
-    acceptableAnswers.put("ca7e7d4d-3406-4684-b049-2adb96678815", Set.of(1));
+		return questions.stream()
+				.map(question -> {
+					List<QuestionOption> options = optionsByQuestionId.getOrDefault(question.getId(),
+							Collections.emptyList());
+					List<String> optionContents = options.stream()
+							.map(QuestionOption::getContent)
+							.collect(Collectors.toList());
 
-    //Monthly food cost
-    acceptableAnswers.put("fa35c3c1-26ae-430d-aacd-128da6388060", Set.of(1, 2, 3, 4));
+					return new QuestionsDto(
+							question.getQuestionType().getName(),
+							question.getId().toString(),
+							question.getContent(),
+							optionContents);
+				})
+				.collect(Collectors.toList());
+	}
 
-    //what im sending back
-    Set<RecommendationDto> recommendationsToReturn = new HashSet<>();
+	/**
+	 * Processes the answers submitted by the user and returns a list of
+	 * recommendations.
+	 * If the answers are not in the pool of answers, then they indicate that the
+	 * user is
+	 * struggling and needs to resources linked to the question.
+	 * 
+	 * @param SubmissionRequestBodyDto The request containing the user's answers.
+	 * @return RecommendationsResponse containing a list of recommendations based on
+	 *         the user's answers.
+	 */
+	public RecommendationsResponse processAnswersAndGetRecommendations(
+			SubmissionRequestBodyDto answerSubmissionRequest) {
 
-    //Retrieving all the recommendations
-    List<Recommendation> databaseRecommendations = recommendationRepository.findAllByOrderByIdAsc();
+		// what im sending back
+		Set<RecommendationDto> recommendationsToReturn = new HashSet<>();
 
-    List<SubmittedAnswerDto> questionAndAnswerPairs = answerSubmissionRequest.getAnswers();
+		// Retrieving all the recommendations
+		List<Recommendation> databaseRecommendations = recommendationRepository.findAllByOrderByIdAsc();
 
-    if (questionAndAnswerPairs != null && !questionAndAnswerPairs.isEmpty()) {
-      for (SubmittedAnswerDto pair : questionAndAnswerPairs) {
-        String questionId = pair.getQuestionId();
-        String answerString = pair.getAnswer();
+		List<SubmittedAnswerDto> questionAndAnswerPairs = answerSubmissionRequest.getAnswers();
 
-        //if user's answer is unnacceptable add to recommendations
-        if (!acceptableAnswers.get(questionId).contains(Integer.valueOf(answerString))) {
-          List<RecommendationDto> dtosForThisQuestion = databaseRecommendations.stream()
-              .filter(rec -> {
-                  if (rec.getQuestion() != null && rec.getQuestion().getId() != null) {
-                      return rec.getQuestion().getId().toString().equals(questionId);
-                  }
-                  return false; 
-              })
-              .map(recEntity -> new RecommendationDto(
-                  recEntity.getQuestion().getId().toString(),
-                  recEntity.getName(),
-                  recEntity.getDescription(),
-                  recEntity.getWebsiteUrl(),
-                  recEntity.getContactUrl()
-              ))
-              .collect(Collectors.toList());
+		if (questionAndAnswerPairs != null && !questionAndAnswerPairs.isEmpty()) {
+			for (SubmittedAnswerDto pair : questionAndAnswerPairs) {
+				String questionId = pair.getQuestionId();
+				String answerString = pair.getAnswer();
 
-          recommendationsToReturn.addAll(dtosForThisQuestion);
-        }
-      }
-    }
-    return new RecommendationsResponse(new ArrayList<>(recommendationsToReturn));
-  }
+				// if user's answer is unnacceptable add to recommendations
+				if (!acceptableAnswers.get(questionId).contains(Integer.valueOf(answerString))) {
+					List<RecommendationDto> dtosForThisQuestion = databaseRecommendations.stream()
+							.filter(rec -> {
+								if (rec.getQuestion() != null && rec.getQuestion().getId() != null) {
+									return rec.getQuestion().getId().toString().equals(questionId);
+								}
+								return false;
+							})
+							.map(recEntity -> new RecommendationDto(
+									recEntity.getQuestion().getId().toString(),
+									recEntity.getName(),
+									recEntity.getDescription(),
+									recEntity.getWebsiteUrl(),
+									recEntity.getContactUrl()))
+							.collect(Collectors.toList());
+
+					recommendationsToReturn.addAll(dtosForThisQuestion);
+				}
+			}
+		}
+		return new RecommendationsResponse(new ArrayList<>(recommendationsToReturn));
+	}
 
 }
